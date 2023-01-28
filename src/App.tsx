2c5ed7243739470ee5803da68cd6d971ba7f9901
Svante Strömvall <svante@tilltopps.se>
2023-01-28 20:06:21 +0100

    Arrange some objects in the scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
-import { Suspense, useRef, useState } from 'react'
+import { Suspense, useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
+import { OrbitControls, Stage, useGLTF } from '@react-three/drei'
 import { Mesh } from 'three'
 import * as Walls from './Walls'
 
 function Astronaut(props: JSX.IntrinsicElements['mesh']) {
   const gltf = useGLTF('https://thinkuldeep.com/modelviewer/Astronaut.glb')
-  return (<primitive {...props} object={gltf.scene} />)
+  return <primitive {...props} object={gltf.scene} />
+}
+
+function GuestroomWalls(props: JSX.IntrinsicElements['group']) {
+  return <Walls.Model scale={0.0001} rotation={[-Math.PI / 2, 0, 0]} />
 }
 
 function Box(props: JSX.IntrinsicElements['mesh']) {
@@ -18,12 +22,12 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
   const [clicked, click] = useState(false)
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame((state, delta) => (ref.current.rotation.x += 0.005))
-  const scale = props.scale as number;
+  const scale = props.scale as number
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5*scale : scale}
+      scale={clicked ? 1.5 * scale : scale}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
@@ -34,14 +38,20 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
 }
 
 export default function App() {
+  const ref = useRef<any>(null!)
   return (
     <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
-      <ambientLight color={0x404040} intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box scale={0.1} position={[1.2, 0, 0]} />
-      <Walls.Model scale={0.0001}  />
-      <Astronaut scale={0.1}/>
+      <Suspense fallback={null}>
+        <Stage preset="rembrandt" intensity={1} environment="city" adjustCamera={0.8}>
+          <ambientLight color={0x404040} intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          <Box scale={0.1} position={[0, 0.1, -1.2]} />
+          <GuestroomWalls />
+          <Astronaut scale={0.1} position={[0.13, 0, -0.7]} rotation={[0, 2, 0]} />
+        </Stage>
+      </Suspense>
+      <OrbitControls ref={ref} autoRotate />
     </Canvas>
   )
 }
