4099dd76ab62aa997e159effe389298f984fc028
Svante Strömvall <svante@tilltopps.se>
2023-01-28 19:08:46 +0100

    Import and render the walls of the guestroom

--- README.md
+++ README.md
@@ -5,5 +5,15 @@
 - React
 - React Three Fiber - a React renderer for three.js https://docs.pmnd.rs/react-three-fiber/
 - TypeScript
-- Volta - JavaScript Tool Manager 
-    https://volta.sh/
+- Volta - JavaScript Tool Manager
+  https://volta.sh/
+
+## How we import GLTF models
+
+Import GLTF models as jsx React Components created by the gltfjsx tool. Run a command like this to create a component:
+
+```sh
+npx gltfjsx walls.gltf 
+```
+
+ Note: this creates a JavaScript file hence allowJs is set to true in tsconfig.json.
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-/* eslint-disable */
-import * as THREE from 'three'
 import * as React from 'react'
-import { useRef, useState } from 'react'
+import { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+import { Mesh } from 'three'
+import * as Walls from './Walls'
+
+function Astronaut(props: JSX.IntrinsicElements['mesh']) {
+  const gltf = useGLTF('https://thinkuldeep.com/modelviewer/Astronaut.glb')
+  return (<primitive {...props} object={gltf.scene} />)
+}
 
 function Box(props: JSX.IntrinsicElements['mesh']) {
   // This reference will give us direct access to the THREE.Mesh object
-  const ref = useRef<THREE.Mesh>(null!)
+  const ref = useRef<Mesh>(null!)
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   // Rotate mesh every frame, this is outside of React without overhead
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-
+  useFrame((state, delta) => (ref.current.rotation.x += 0.005))
+  const scale = props.scale as number;
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
+      scale={clicked ? 1.5*scale : scale}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
@@ -29,12 +35,13 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
 
 export default function App() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
+    <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+      <ambientLight color={0x404040} intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box scale={0.1} position={[1.2, 0, 0]} />
+      <Walls.Model scale={0.0001}  />
+      <Astronaut scale={0.1}/>
     </Canvas>
   )
 }
