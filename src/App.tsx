6aa09cfd323b6d091fa908c01d1a43665de74372
Svante Strömvall <svante@tilltopps.se>
2023-01-28 22:12:31 +0100

    Add part of a floor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { Suspense, useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Stage, useGLTF } from '@react-three/drei'
-import { Mesh } from 'three'
+import { OrbitControls, Stage, useGLTF, useTexture } from '@react-three/drei'
+import { DoubleSide, Mesh } from 'three'
 import * as Walls from './Walls'
 import * as BedKing from './BedKing'
 import * as Couch from './Couch'
@@ -20,6 +20,21 @@ function Astronaut(props: JSX.IntrinsicElements['mesh']) {
 
 function GuestroomWalls(props: JSX.IntrinsicElements['group']) {
   return <Walls.Model scale={0.0001} rotation={[-Math.PI / 2, 0, 0]} />
+}
+
+function GuestroomFloor(props: JSX.IntrinsicElements['mesh']) {
+  const texture = useTexture({
+    map: 'Tiles001_1K_Color.jpg',
+    displacementMap: 'Tiles001_1K_Displacement.jpg',
+    normalMap: 'Tiles001_1K_NormalGL.jpg',
+    roughnessMap: 'Tiles001_1K_Roughness.jpg',
+  })
+  return (
+    <mesh {...props}>
+      <planeGeometry args={[3, 5]} />
+      <meshStandardMaterial {...texture} displacementScale={0.02} side={DoubleSide} />
+    </mesh>
+  )
 }
 
 function Box(props: JSX.IntrinsicElements['mesh']) {
@@ -56,6 +71,7 @@ export default function App() {
           <pointLight position={[-10, -10, -10]} />
           {/* <Box scale={0.1} position={[0, 0.1, -1.2]} /> */}
           <GuestroomWalls />
+          <GuestroomFloor scale={0.05} position={[0.07, 0, -0.40]} rotation={[-Math.PI / 2, 0, 0]} />
           <group name="Entrance">
             <Couch.Model scale={0.0031} position={[0.73, 0, 0.38]} rotation={[0, Math.PI / 2, 0]} />
           </group>
